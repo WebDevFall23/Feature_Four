20e5a87cddb3cd7818c40c307be8a294aa0eedc4
EmelieMadrigal01 <97383325+EmelieMadrigal01@users.noreply.github.com>
2023-10-05 16:31:22 -0400

    Compenents file

---
+++ feat4/src/Components/Components.js
@@ -0,0 +1,16 @@
+import Home from "./Home/Home";
+import Library from "./Library/Library";
+import NavBar from "./NavBar/NavBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+export default function Components() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<Library />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
