8f284b459d03d219a4e028846112669b50579527
makennagall <68980127+makennagall@users.noreply.github.com>
2023-11-05 12:02:26 -0500

    Auth module added

--- feat4/src/Components/Components.js
+++ feat4/src/Components/Components.js
@@ -3,6 +3,8 @@ import Profile from "./Profile/Profile";
 import UserPlant from "./UserPlant/UserPlant";
 import NavBar from "./NavBar/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuthLogin from "./Auth/AuthLogin"
+import AuthRegister from "./Auth/AuthRegister"
 
 export default function Components() {
   return (
@@ -11,6 +13,8 @@ export default function Components() {
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/userplant" element={<UserPlant />} />
+        <Route path="/login" element={<AuthLogin />} />
+        <Route path="/register" element={<AuthRegister />} />
       </Routes>
       <NavBar />
     </Router>
--- feat4/src/Components/NavBar/NavBar.js
+++ feat4/src/Components/NavBar/NavBar.js
@@ -14,12 +14,21 @@ export default function NavBar() {
   const buttonHandlerUserPlant = () => {
     history("/UserPlant");
   };
+  const buttonHandlerLogin = () => {
+    history("/login");
+  };
+  const buttonHandlerRegister = () => {
+    history("/register");
+  };
   return (
     <section>
         <ul>
             <li><button onClick={buttonHandlerHome}>Home</button></li>
             <li><button onClick={buttonHandlerProfile}>Profile</button></li>
             <li><button onClick={buttonHandlerUserPlant}>UserPlant</button></li>
+            <li><button onClick={buttonHandlerLogin}>Login</button></li>
+            <li><button onClick={buttonHandlerRegister}>Register</button></li>
+
         </ul>
     </section>
   );
