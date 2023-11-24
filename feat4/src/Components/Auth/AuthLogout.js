003b7e9358c62ebc78bb584359bf0253057e8219
makennagall <68980127+makennagall@users.noreply.github.com>
2023-11-23 20:58:17 -0500

    logout working

---
+++ feat4/src/Components/Auth/AuthLogout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Parse from 'parse';
+import { useNavigate } from "react-router-dom";
+
+
+const UserLogOut = () => {
+  const navigate = useNavigate();
+
+  const logoutUser = async function () {
+    return await Parse.User.logOut()
+      .then(async () => {
+        // To verify that current user is now empty, currentAsync can be used
+        const currentUser = await Parse.User.currentAsync();
+        if (currentUser === null) {
+          alert('Success!', 'No user is logged in anymore!');
+        }
+        // Navigate back to login screen when successfully loggedout
+        navigate('/auth/login');
+        return true;
+      })
+      .catch((error) => {
+        alert('Error!', error.message);
+        return false;
+      });
+  };
+
+  return (
+          <button onClick={logoutUser}>Logout</button>
+  );
+};
+
+export default UserLogOut;
--- feat4/src/Components/Profile/Profile.js
+++ feat4/src/Components/Profile/Profile.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ProfileList from "./ProfileList";
 import { Link } from "react-router-dom";
+import UserLogOut from "../Auth/AuthLogout"
 
 /* Profile MODULE WITH STATEFUL PARENT AND STATELESS CHILD */
 const ProfileModule = () => {
   return (
     <div>
+      <div>
+        <UserLogOut/>
+      </div>
         <h1>This is the Profile module.</h1>
         <ProfileList />
         <br />
-      <Link to="/navbar">
+      <Link to="/auth/login">
         <button>Back</button>
       </Link>
     </div>
